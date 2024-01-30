c49423cbd4f49ddfab12cdd205f31d1463672900
tegarjgap <tegarjagatgeni@gmail.com>
2024-01-30 21:14:44 +0700

    feat: added capture photo func

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef } from "react";
+import "./App.css";
 
 function App() {
+  const videoElement = useRef(null);
+  const canvasElement = useRef(null);
+
+  useEffect(() => {
+    const startCamera = async () => {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: false,
+      });
+      videoElement.current.srcObject = stream;
+    };
+    startCamera();
+  }, []);
+
+  const capturePhoto = () => {
+    canvasElement.current
+      .getContext("2d")
+      .drawImage(
+        videoElement.current,
+        0,
+        0,
+        canvasElement.current.width,
+        canvasElement.current.height
+      );
+    const imageURL = canvasElement.current.toDataURL("image/jpeg");
+    console.log(imageURL);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        gap: "20px",
+      }}
+    >
+      <div>
+        <video ref={videoElement} width="320" height="300" autoPlay></video>
+      </div>
+      <div>
+        <button id="click-photo" onClick={capturePhoto}>
+          Capture
+        </button>
+      </div>
+      <div>
+        <canvas ref={canvasElement} width="320" height="240"></canvas>
+      </div>
     </div>
   );
 }
